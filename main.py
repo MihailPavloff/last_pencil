685521d32b1012a018fb31fb5dd30f0207c03902
MihailPavloff <87079189+MihailPavloff@users.noreply.github.com>
2022-09-29 01:33:49 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("How many pencils would you like to use:")
+pencil_amount = int(input())
+
+print("Who will be the first (Vici, Friday):")
+first_player = input()
+
+print(pencil_amount * "|")
+print(f'{first_player} is going first!')
