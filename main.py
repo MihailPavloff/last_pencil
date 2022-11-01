65b31b889f4dce7eef2384a29e4a7954ba46a49b
MihailPavloff <87079189+MihailPavloff@users.noreply.github.com>
2022-11-01 13:20:36 +0300

    Stage 4

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ def init():
     print("How many pencils would you like to use:")
     while True:
         amount = input()
-        for sym in amount:
+        error_flag = 0
+        if amount.lstrip('-') == "" or amount.lstrip("+") == "":
+            print("The number of pencils should be numeric")
+            continue
+        for sym in amount.lstrip('-'):
             if sym not in string.digits:
                 print("The number of pencils should be numeric")
-        if amount <= 0:
+                error_flag = 1
+        if error_flag:
+            continue
+        elif int(amount) <= 0:
             print("The number of pencils should be positive")
+            continue
         else:
             break;
 
@@ -19,12 +27,13 @@ def init():
         name = input()
         if name not in players:
             print(f'Choose between {players[0]} and {players[1]}')
+            continue
         break
     if name == players[0]:
         index = 0
     else:
         index = 1
-    return amount, index
+    return int(amount), index
 
 
 possible_values = (1, 2, 3)
@@ -33,12 +42,19 @@ pencil_amount, current_player = init()
 while pencil_amount > 0:
     print(pencil_amount * "|")
     print(f"{players[current_player]}'s turn:")
-    current_player = (current_player + 1) % 2
     taking_pencils = 0
-    while taking_pencils not in possible_values and taking_pencils <= pencil_amount:
-        taking_pencils = int(input())
-        if taking_pencils not in (1, 2, 3):
+    while True:
+        taking_pencils = input()
+        if taking_pencils not in '123':
             print("Possible values: '1', '2' or '3'")
-        elif taking_pencils > pencil_amount:
+            continue
+        elif int(taking_pencils) > pencil_amount:
             print("Too many pencils were taken.")
+            continue
+        taking_pencils = int(taking_pencils)
+        break
     pencil_amount -= taking_pencils
+    if pencil_amount == 0:
+        winner = (current_player + 1) % 2
+        print(f"{players[winner]} won!")
+    current_player = (current_player + 1) % 2
